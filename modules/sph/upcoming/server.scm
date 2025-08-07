46e17d47925ac9a4c84467c6f5cd1a6872608f4d
sph <sph@posteo.eu>
2025-08-07 13:23:31 +0200

    use guile module syntax

--- modules/sph/upcoming/server.scm
+++ modules/sph/upcoming/server.scm
@@ -1,51 +1,40 @@
-(library (sph upcoming server)
-  (export
-    upcoming-client
-    upcoming-client-query
-    upcoming-client-upcoming
-    upcoming-server
-    upcoming-server-path)
-  (import
-    (guile)
-    (ice-9 match)
-    (rnrs eval)
-    (sph)
-    (sph alist)
-    (sph io)
-    (sph list)
-    (sph other)
-    (sph server)
-    (sph time)
-    (sph upcoming))
+(define-module (sph upcoming server))
 
-  (define upcoming-server-path "/tmp/upcoming-server")
+(use-modules (guile) (ice-9 match)
+  (rnrs eval) (sph)
+  (sph alist) (sph io) (sph list) (sph other) (sph server) (sph time) (sph upcoming))
 
-  (define (upcoming-server-upcoming cc time past-n active-n future-n id-n ids)
-    (upcoming (config-cache-update cc) time past-n active-n future-n #:id-n id-n #:ids ids))
+(export upcoming-client upcoming-client-query
+  upcoming-client-upcoming upcoming-server upcoming-server-path)
 
-  (define (respond cc a)
-    (match a (((quote upcoming) a ...) (apply upcoming-server-upcoming cc a))
-      (else (list (q invalid-query) a))))
+(define upcoming-server-path "/tmp/upcoming-server")
 
-  (define* (upcoming-server #:optional path config-path)
-    (let
-      ( (cc (upcoming-config-cached config-path))
-        (socket (server-socket (or path upcoming-server-path))))
-      (display-line
-        (string-append "start listening on " (or path upcoming-server-path) "\nexit with ctrl+c"))
-      (server-listen (l (client) (put-datum client (respond cc (get-datum client)))) socket
-        #:parallelism 1)))
+(define (upcoming-server-upcoming cc time past-n active-n future-n id-n ids)
+  (upcoming (config-cache-update cc) time past-n active-n future-n #:id-n id-n #:ids ids))
 
-  (define* (upcoming-client proc #:optional path)
-    (let*
-      ( (s (socket PF_UNIX SOCK_STREAM 0))
-        (result (and (connect s AF_UNIX (or path upcoming-server-path)) (proc s))))
-      (close-port s) result))
+(define (respond cc a)
+  (match a (((quote upcoming) a ...) (apply upcoming-server-upcoming cc a))
+    (else (list (q invalid-query) a))))
 
-  (define (upcoming-client-query query)
-    (upcoming-client (l (server) (put-datum server query) (get-datum server))))
+(define* (upcoming-server #:optional path config-path)
+  (let
+    ( (cc (upcoming-config-cached config-path))
+      (socket (server-socket (or path upcoming-server-path))))
+    (display-line
+      (string-append "start listening on " (or path upcoming-server-path) "\nexit with ctrl+c"))
+    (server-listen (l (client) (put-datum client (respond cc (get-datum client)))) socket
+      #:parallelism 1)))
 
-  (define* (upcoming-client-upcoming #:optional time past-n active-n future-n id-n ids)
-    (upcoming-client-query
-      (list (q upcoming) (or time (utc-current))
-        (or past-n 0) active-n (or future-n 1) id-n (or ids null)))))
+(define* (upcoming-client proc #:optional path)
+  (let*
+    ( (s (socket PF_UNIX SOCK_STREAM 0))
+      (result (and (connect s AF_UNIX (or path upcoming-server-path)) (proc s))))
+    (close-port s) result))
+
+(define (upcoming-client-query query)
+  (upcoming-client (l (server) (put-datum server query) (get-datum server))))
+
+(define* (upcoming-client-upcoming #:optional time past-n active-n future-n id-n ids)
+  (upcoming-client-query
+    (list (q upcoming) (or time (utc-current))
+      (or past-n 0) active-n (or future-n 1) id-n (or ids null))))
